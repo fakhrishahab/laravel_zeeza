7e53809001e2369cc7a2b2593f85a63634c9b26a
fakhrishahab <fakhri1804@gmail.com>
2016-02-11 17:33:06 +0700

    update

--- app/Http/Models/category.php
+++ app/Http/Models/category.php
@@ -9,11 +9,11 @@ class category extends Model
 {
 	protected $table = "product_category";
 	function get_data(){
-		return DB::table('product_category')->get();
+		return DB::table('product_category')->orderBy('id_category')->get();
 	}
 
 	function get_type($id){
-		return DB::table('product_type')->where('id_category', $id)->get();	
+		return DB::table('product_type')->where('id_category', $id)->orderBy('id_type')->get();	
 	}
 
 	function get_size(){
